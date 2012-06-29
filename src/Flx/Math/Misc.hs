7eaadd5a0f5a744e72c1382b08b62d538de88a7c
Felix C. Stegerman <flx@obfusk.net>
2012-06-30 00:22:18 +0200

    ~ Flx{,.Math}.Misc (tuples; sqrt/conv)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- src/Flx/Math/Misc.hs
+++ src/Flx/Math/Misc.hs
@@ -19,8 +19,12 @@
 --  --                                                            }}}1
 
 module Flx.Math.Misc (                                        --  {{{1
-  sqrtContFrac
+  sqrtContFrac, sqrtContConv, convergents, convergents'
 ) where                                                       --  }}}1
+
+--
+
+import Data.List (zipWith4)
 
 --
 
@@ -34,5 +38,20 @@ sqrtContFrac s
         d'  = (s - m'^2) `div` d
         a'  = (a0 + m') `div` d'
     a0 = floor . sqrt $ fromIntegral s
+
+sqrtContConv :: Integral a => a -> [(a,a)]
+sqrtContConv = convergents' . sqrtContFrac
+
+convergents :: Integral a => [a] -> [a] -> [(a,a)]
+convergents (a1:at) (b0:b1:bt)
+    = cs
+  where
+    cs = (x0,y0) : (x1,y1) : zipWith4 next cs (tail cs) at bt
+    x0 = b0         ; y0 = 1
+    x1 = b1*b0 + a1 ; y1 = b1
+    next (x,y) (x',y') a b = (b*x' + a*x,b*y' + a*y)
+
+convergents' :: Integral a => [a] -> [(a,a)]
+convergents' = convergents (repeat 1)
 
 -- vim: set tw=70 sw=2 sts=2 et fdm=marker :
--- src/Flx/Misc.hs
+++ src/Flx/Misc.hs
@@ -2,7 +2,7 @@
 --
 --  File        : Flx/Misc.hs
 --  Maintainer  : Felix C. Stegerman <flx@obfusk.net>
---  Date        : 2012-06-28
+--  Date        : 2012-06-29
 --
 --  Copyright   : Copyright (C) 2012  Felix C. Stegerman
 --  Licence     : GPLv2
@@ -15,7 +15,7 @@
 --  --                                                            }}}1
 
 module Flx.Misc (                                             --  {{{1
-  (-!)
+  (-!), swap, fst3, snd3, trd3, fst4, snd4, trd4, fth4
 ) where                                                       --  }}}1
 
 --
@@ -24,5 +24,28 @@ module Flx.Misc (                                             --  {{{1
 (-!) = (-)
 
 infixl 6 -!
+
+--
+
+swap :: (a,b) -> (b,a)
+swap (x,y) = (y,x)
+
+fst3 :: (a,b,c) -> a
+snd3 :: (a,b,c) -> b
+trd3 :: (a,b,c) -> c
+
+fst3 (a,_,_) = a
+snd3 (_,b,_) = b
+trd3 (_,_,c) = c
+
+fst4 :: (a,b,c,d) -> a
+snd4 :: (a,b,c,d) -> b
+trd4 :: (a,b,c,d) -> c
+fth4 :: (a,b,c,d) -> d
+
+fst4 (a,_,_,_) = a
+snd4 (_,b,_,_) = b
+trd4 (_,_,c,_) = c
+fth4 (_,_,_,d) = d
 
 -- vim: set tw=70 sw=2 sts=2 et fdm=marker :
